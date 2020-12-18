fc77731888030099f4336dad55c72c016eb5ac87
unknown <hongocdangkhoa555@gmail.com>
2020-12-18 22:23:56 +0700

    baitap

---
+++ Module_1/Bai_11_Ham/Baitap/my_library.js
@@ -0,0 +1,14 @@
+function isPrimeNumber(number) {
+    let count = 0;
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            count++;
+        }
+    }
+
+    if (count === 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
