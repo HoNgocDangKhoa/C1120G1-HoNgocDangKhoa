4091fe9d54373fe800929622688eb9844ab7b48a
unknown <hongocdangkhoa555@gmail.com>
2021-02-26 11:29:57 +0700

    update

--- Module_3/bai2_thiet_ke_tao_csdl/baitap/baitap_phantich_thietke_csdl.sql
+++ Module_3/bai2_thiet_ke_tao_csdl/baitap/baitap_phantich_thietke_csdl.sql
@@ -1,5 +1,29 @@
 create database bai_phantich_thietke_csdl;
 use bai_phantich_thietke_csdl;
+create table productlines 
+(productLine varchar(50)primary key,
+textDescription text,
+image varchar(50)
+);
+create table offices 
+(officeCode varchar(10) primary key,
+city varchar(50) not null,
+phone varchar(50) not null,
+addressLine1 varchar(50) not null,
+addressLine2 varchar(50) not null,
+state  varchar(50),
+country varchar(50) not null,
+postalCode varchar(15)not null
+);
+create table employees 
+(employeeNumber int primary key,
+lastName varchar(50) not null,
+firstName varchar(50) not null,
+email varchar(100) not null,
+jobTitle varchar(50) not null,
+officeCode varchar(10),
+foreign key(officeCode) references offices(officeCode)
+);
 create table customer
 (customerNumber int primary key,
 customerName varchar(50) not null,
@@ -12,7 +36,11 @@ city varchar(50) not null,
 state varchar(50) not null,
 postalCode varchar(15) not null,
 country varchar(50) not null,
-creditLimit double
+creditLimit double,
+employeeNumber int ,
+foreign key(employeeNumber) references employees(employeeNumber),
+reportTo int,
+foreign key (reportTo )  references customer(customerNumber)
 );
  create table orders 
  (orderNumber int primary key,
@@ -41,33 +69,15 @@ foreign key (customerNumber) references customer(customerNumber)
  productDescription text not null,
  quantityInStock int not null,
  buyPrice double not null,
- MSRP double not null
+ MSRP double not null,
+ productLine varchar(50),
+ foreign key(productLine) references productlines(productLine)
  );
-create table productlines 
-(productLine varchar(50)primary key,
-textDescription text,
-image varchar(50)
-);
-create table employees 
-(employeeNumber int primary key,
-lastName varchar(50) not null,
-firstName varchar(50) not null,
-email varchar(100) not null,
-jobTitle varchar(50) not null
-);
-create table offices 
-(officeCode varchar(10) primary key,
-city varchar(50) not null,
-phone varchar(50) not null,
-addressLine1 varchar(50) not null,
-addressLine2 varchar(50) not null,
-state  varchar(50),
-country varchar(50) not null,
-postalCode varchar(15)not null
-);
 create table OrderDetails 
 (orderNumber int,
 productCode varchar(50),
-primary key (orderNumber, productCode)
+primary key (orderNumber, productCode),
+foreign key(productCode) references products(productCode),
+foreign key (orderNumber) references orders(orderNumber)
 );
 
